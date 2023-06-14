0d7041c8a61ba4eae0fbf478ade90dacc8f1decc
Purvish <prathammangroliya60@gmail.com>
2023-06-14 08:37:56 +0530

    FInal review has been done.
    Signed-off-by: Purvish <prathammangroliya60@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page routes. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET contact page. */
+/* GET contact page routes. */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
 
-/* GET projects page. */
+/* GET projects page routes. */
 router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects' });
 });
 
-/* GET about page. */
+/* GET about page routes. */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
